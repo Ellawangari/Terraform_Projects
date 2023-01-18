764927f2f5eae98b13bf9068f4e0e15f5ddb31d5
Ellawangari <elamaina8@gmail.com>
2023-01-18 04:22:55 +0000

    terraform functions

---
+++ Functions/main.tf
@@ -0,0 +1,30 @@
+resource "aws_key_pair" "devopskey"{
+
+key_name = "devops-key"
+public_key = file("${path.module}/id_rsa.pub")
+
+
+}
+
+resource "aws_instance" "appserver"{
+  //loookup retrieves a value from a map given the key , so in our case the 
+  //The map is var.ami then the key is var.region
+  //if the ke is none existant then it picks the default value
+ ami = lookup(var.ami, var.region)
+ instance_type = var.instancetype
+ key_name = aws_key_pair.devopskey.key_name  
+ count = 3
+ 
+
+ tags = {
+
+//element retrieves a single element from  a list
+// element(list,index)
+   Name = element(var.instances, count.index)
+
+ }
+
+
+
+
+}
---
+++ Functions/terraform.tfvars
@@ -0,0 +1,5 @@
+# instancetype ="t2.large"
+# region= "us-west-1"
+availability-zones = ["us-east-1", "us-west-1"]
+
+istest = false

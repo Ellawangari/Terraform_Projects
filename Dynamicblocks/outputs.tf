e4880415705b4afaa0e074ce2989b8fcae51afcc
Ellawangari <elamaina8@gmail.com>
2023-01-21 06:37:40 +0000

    zipmap terraform function

--- Dynamicblocks/outputs.tf
+++ Dynamicblocks/outputs.tf
@@ -4,3 +4,18 @@ output "devuserarn" {
   value = aws_iam_user.devuser[*].arn
 
 }
+
+
+output "devusernames"{
+
+value = aws_iam_user.devuser[*].name
+
+}
+
+//zipmap function
+
+output "devarn-names"{
+
+
+  value = zipmap(aws_iam_user.devuser[*].name, aws_iam_user.devuser[*].arn)
+}

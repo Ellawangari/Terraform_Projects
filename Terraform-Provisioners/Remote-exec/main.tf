067e3c73153e31b3e30af9505acde4aa7434e396
Ellawangari <elamaina8@gmail.com>
2023-01-24 08:00:32 +0000

    remote exec changes

---
+++ Terraform-Provisioners/Remote-exec/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "test-ec2" {
+  //creating the instance
+  instance_type          = var.instance-type
+  ami                    = var.ami
+  key_name               = "terraform-prac"
+  vpc_security_group_ids = [aws_security_group.allowhttp.id]
+
+  tags = {
+
+    Name = "test-ec2"
+
+
+  }
+
+  //Provisioner connection
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    agent = false
+    private_key = file("./terraform-prac.pem")
+    host        = self.public_ip
+  }
+
+
+  //Provisioner
+
+  provisioner "remote-exec" {
+    inline = [
+      //Since its an ubuntu instance we want to install nginx and start it
+      "sudo apt update",
+      "sudo apt install nginx -y "
+
+    ]
+  }
+
+
+}
--- Terraform-Provisioners/Remote-exec/variables.tf
+++ Terraform-Provisioners/Remote-exec/variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "ami" {
 
 
-  default = "ami-0b5eea76982371e91"
+  default = "ami-0778521d914d23bc1"
 }
 
 variable "instance-type" {
@@ -14,11 +14,21 @@ variable "instance-type" {
   default = "t2.micro"
 }
 
-
+//inbound rules ports
 variable "sg_ports" {
 
   type        = list(number)
   description = "list of inbound ports"
   default     = [22, 80, 443]
+
+}
+
+//outbound rules port
+
+variable "egress_ports" {
+
+  type        = list(number)
+  description = "list of inbound ports"
+  default     = [65535]
 
 }
